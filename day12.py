e374db497eb1adbb40cbc8186bdc86950c6ab3a1
Theekshani1103 <theekshani1104@gmail.com>
2026-04-03 20:31:55 +0530

    Pattern programs

---
+++ day12.py
@@ -0,0 +1,90 @@
+'''
+Pattern programming
+num = int(input("Enter the limit:"))
+for j in range(num):
+    for i in range(num):
+        print("*",end = "")#square box
+    print()'''
+
+'''num = int(input("Enter the limit:"))
+for j in range(num):
+    for i in range(j):
+        print(i,end = "")#pattern
+    print()'''
+
+'''num = int(input("Enter the limit:"))
+for j in range(num,0,-1):
+    for i in range(j):
+        print("*",end = "")
+    print()
+#reverse pattern
+(or)
+num = int(input("Enter the limit:"))
+for j in range(num):
+    for i in range(num-j):
+        print("*",end = "")
+    print()'''
+
+'''num = int(input("Enter the limit:"))
+for j in range(num):
+        print(" " *(num-j),end = "")#pyramid
+        for i in range(j+1):
+            print("*", end = " ")
+        print()'''
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+            
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
