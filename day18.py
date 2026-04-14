4d80727bd923f9eec87ba4e1475368d208f3f8ba
Theekshani1103 <theekshani1104@gmail.com>
2026-04-14 19:29:42 +0530

    Modules

---
+++ day18.py
@@ -0,0 +1,60 @@
+'''
+modules
+-------
+--> a module in a python is simple file that contain python code(functions,variables,classes)
+--> To use modules , we have to use a keyword called import before the module
+
+types of modules
+----------------
+1) Built-in or Inbuilt
+2) User-define modules
+
+
+user - define module
+--------------------
+--> This is developed by the user or programer inside a file or python code and used by called import with filename
+syntax --> import(keyword) file_name
+           file_name.functionality
+
+
+import my_module
+print(my_module.add(29,11))
+
+import my_module
+print(my_module.num+11)
+
+import day11
+print(day11.num)
+
+
+Built-in or Inbuilt
+-------------------
+--> Already these are comes with installation  and they are ready to use in the program
+--> This is developed by the developer
+example :- OS,Math.....
+
+syntax --> import(keyword) module_name
+           module_name.functionality
+
+import math
+print(math.sqrt(29))'''
+
+import random
+print(randit(11,29)):
+    for i in randit:
+        guessing_val = int(input("Enter guessing_val:")
+                           if 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
