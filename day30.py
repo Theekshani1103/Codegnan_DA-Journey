991c0232144ac1b5701e87d046c58aab62e7b38f
Theekshani1103 <theekshani1104@gmail.com>
2026-04-29 14:34:22 +0530

    Introduction to Libraries

---
+++ day30.py
@@ -0,0 +1,66 @@
+'''
+Data Analysis:
+why this is a need?
+--> This is critical bcoz it converts raw data into actionable insights,
+enabling information to decision making easy and improve operational
+efficiency....
+1)Decision-making
+2)Improved operational efficiency
+3)Customer understanding
+4)Market Insight
+5)Risk Management
+6)Data-Driven Strategies
+
+#line plot
+import matplotlib.pyplot as pip
+X = [1,2,3,4,5]
+Y = [6,7,8,9,4]
+pip.plot(X,Y)
+pip.show()
+
+#Bar graph
+import matplotlib.pyplot as pip
+pip.bar(["TV9","NDTV","BBC"],[4,8,10])
+pip.show()
+
+#pie chart
+import matplotlib.pyplot as pip
+pip.pie([40,60,50], labels = ["week1","week2","week3"])
+pip.show()
+
+#Histogram
+import matplotlib.pyplot as pip
+pip.hist([10,20,30,40])
+pip.show()
+
+Numpy
+=====
+--> Numpy(numerical python) this is foundational open-source library
+for scientific computing python, providing high performance,N-dimensional arrays objects(ndarray)
+--> This enables efficient numerical computing with linear algebra, and data manipulation 
+
+import numpy as np
+arr = np.array([1,2,3])
+print(arr-1)
+
+pandas
+======
+--> This pandas is used for handling structured data in table format
+
+import pandas as pd
+data = {"Name" : ["Theeku","Honey"],"Marks" : [75,80]}
+any = pd.DataFrame(data)#to get data in table format we should use DataFrame
+print(any)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
